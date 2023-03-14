ef9e3428ab03fdb1d7335dbd4219797b00892588
JeaWoo-Park <jjjj901913@gmail.com>
2023-03-14 15:21:48 +0900

    3/13 수업 내용

--- ServerTogether/ServerTogether/ServerTogether.cpp
+++ ServerTogether/ServerTogether/ServerTogether.cpp
@@ -1,20 +1,55 @@
-﻿// ServerTogether.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+using namespace chrono;
+
+// Cache Miss
+// 
+//constexpr int CACHE_LINE_SIZE = 32;
+//int main()
+//{
+//	for (int i = 0; i < 20; ++i) {
+//		const int size = 1024 << i;
+//		long long* a = (long long*)malloc(size);
+//		unsigned int index = 0;
+//		long long tmp = 0;
+//		const int num_data = size / 8;
+//		auto start = high_resolution_clock::now();
+//		for (int j = 0; j < 100000000; ++j) {
+//			tmp += a[index % num_data];
+//			index += CACHE_LINE_SIZE * 11;
+//		}
+//		auto dur = high_resolution_clock::now() - start;
 //
+//		auto ms = duration_cast<milliseconds>(dur).count();
+//		cout << "MEM size = " << size / 1024 << "KB, ";
+//		cout << "Exec time = " << ms << "ms, ";
+//		cout << "Result = " << tmp << std::endl;
+//		free(a);
+//	}
+//}
 
-#include <iostream>
-
+// 분기없이 하는 것이 훨씬 성능이 좋다 abs보다 abs2가 더 빠름
+#define abs(x) (((x>0)?(x):(-x)))
+#define abs2(x) (((x>>31)^x) - (x>>31))
+constexpr int T_SIZE = 100000000;
+short rand_arr[T_SIZE];
 int main()
 {
-    std::cout << "Hello World!\n";
+	int sum;
+	for (int i = 0; i < T_SIZE; ++i) rand_arr[i] = rand() - 16384;
+	sum = 0;
+	auto start_t = high_resolution_clock::now();
+	for (int i = 0; i < T_SIZE; ++i) sum += abs(rand_arr[i]);
+	auto du = high_resolution_clock::now() - start_t;
+	cout << "[abs] Time " << duration_cast<milliseconds>(du).count() << " ms\n";
+	cout << "Result : " << sum << endl;
+	sum = 0;
+	start_t = high_resolution_clock::now();
+	for (int i = 0; i < T_SIZE; ++i) sum += abs2(rand_arr[i]);
+	du = high_resolution_clock::now() - start_t;
+	cout << "[abs2] Time " << duration_cast<milliseconds>(du).count() << " ms\n";
+	cout << "Result : " << sum << endl;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
